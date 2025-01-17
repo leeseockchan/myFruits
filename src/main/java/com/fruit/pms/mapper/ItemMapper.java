fca00fa9eef96fbef2630f42485f6b3671892036
leeseokchan <dltrjcks97@gmail.com>
2025-01-17 10:51:48 +0900

    Mapper - update, delete 추가

--- src/main/java/com/fruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/fruit/pms/mapper/ItemMapper.java
@@ -3,9 +3,14 @@ package com.fruit.pms.mapper;
 import com.fruit.pms.dto.ItemDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ItemMapper {
 
     void insertItem(ItemDto itemDto);
     ItemDto getItemById(int id);
+    List<ItemDto> getItems();
+    void updateItem(ItemDto itemDto);
+    void deleteItem(int id);
 }
